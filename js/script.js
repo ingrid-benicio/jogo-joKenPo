ea8159aafab2db86a0be502202a4ca93cf5001c7
Ingrid Benicio <78183082+ingrid-benicio@users.noreply.github.com>
2022-05-11 19:23:47 -0300

    Update JS * Escolha da CPU
    Update com a escolha da CPU no alert().

--- js/script.js
+++ js/script.js
@@ -19,15 +19,15 @@ function jogoEmSi (){
     if (document.getElementById("escolha").value == computador ){
         alert("Deu empate!");
     }else if( document.getElementById("escolha").value =="Pedra" && computador == "Papel" ){
-        alert("A CPU ganhou! :/ ");
+        alert("A CPU ganhou! Escolha da CPU: " + computador);
     }else if(document.getElementById("escolha").value == "Papel" && computador == "Pedra"){
         alert("Parabéns, você ganhou!");
     }else if(document.getElementById("escolha").value == "Papel" && computador == "Tesoura"){
-        alert("A CPU ganhou! :/");
+        alert("A CPU ganhou! Escolha da CPU: " + computador);
     }else if (document.getElementById("escolha").value == "Tesoura" && computador == "Papel"){
         alert("Parabéns, você ganhou!");
     }else if (document.getElementById("escolha").value == "Tesoura" && computador == "Pedra"){
-        alert("A CPU ganhou! :/");
+        alert("A CPU ganhou! Escolha da CPU: " + computador);
     }else if (document.getElementById("escolha").value == "Pedra" && computador == "Tesoura"){
         alert("Parabens, você ganhou!");
     }else{
