e48770bba280e0e7cf51a496903ec26014f1ae0e
Ingrid Benicio <78183082+ingrid-benicio@users.noreply.github.com>
2022-05-11 15:44:59 -0300

    Criação do HTML,CSS e JS.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let computador = Math.random() * 27;
+
+if (computador <= 9){
+    computador = "Pedra";
+}else if ((computador >= 10) && computador <=18){
+    computador = "Papel";
+}else{
+    computador = "Tesoura";
+};
+
+
+
+
+
+
+document.getElementById("botaoEnviar").addEventListener("click",jogoEmSi);
+
+function jogoEmSi (){
+    if (document.getElementById("escolha").value == computador ){
+        alert("Deu empate!");
+    }else if( document.getElementById("escolha").value =="Pedra" && computador == "Papel" ){
+        alert("A CPU ganhou! :/ ");
+    }else if(document.getElementById("escolha").value == "Papel" && computador == "Pedra"){
+        alert("Parabéns, você ganhou!");
+    }else if(document.getElementById("escolha").value == "Papel" && computador == "Tesoura"){
+        alert("A CPU ganhou! :/");
+    }else if (document.getElementById("escolha").value == "Tesoura" && computador == "Papel"){
+        alert("Parabéns, você ganhou!");
+    }else if (document.getElementById("escolha").value == "Tesoura" && computador == "Pedra"){
+        alert("A CPU ganhou! :/");
+    }else if (document.getElementById("escolha").value == "Pedra" && computador == "Tesoura"){
+        alert("Parabens, você ganhou!");
+    }else{
+        alert("Digite uma das opções: Pedra, Papel ou Tesoura.");
+    };
+};
+
+
+
